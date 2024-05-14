044ef91cf340cadf1eb0c83ff0236adee164f2a0
axlothecook <alliston.ruby123@gmail.com>
2024-05-14 20:08:31 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,38 @@
+//check if object is empty or not
+// let schedule = {};
+
+// function isEmpty(obj){
+//     for(let key in obj){
+//         return false;
+//     }
+//     return true;
+// };
+
+// console.log(isEmpty(schedule));
+
+//calculator
+// function adding(num, num2){return parseInt(num) + parseInt(num2);}
+// function substraction(num, num2){return parseInt(num) - parseInt(num2);}
+// function multiply(num, num2){return parseInt(num) * parseInt(num2);}
+// function division(num, num2){return parseInt(num) / parseInt(num2);}
+
+// let number, operator, number2;
+
+// const numObject = {
+//     '+': adding(number, number2), 
+//     '-': substraction(number, number2), 
+//     '*': multiply(number, number2),
+//     '/': division(number, number2)
+// }
+
+// do{
+//     while(number != '=' || operator != '=' || number2 != '='){
+//     number = window.prompt("Enter a number: ");
+//     operator = window.prompt("Enter an operator: ");
+//     number2 = window.prompt("Enter a number: ");
+//     operator = window.prompt("Enter an operator: ");
+//     }
+//     console.log(numObject[operator]);
+// }while(number != 0);
+
+
