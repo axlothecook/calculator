1cd18aa76b357c99a3e0199af7250fa8e6f26403
axlothecook <alliston.ruby123@gmail.com>
2024-05-18 11:32:05 +0200

    fixing logic

--- script.js
+++ script.js
@@ -1,56 +1,96 @@
-//check if object is empty or not
-// let schedule = {};
+let display = document.getElementById('screen');
+let parent = document.querySelector('.btn-container');
+let clear = document.querySelector('.clear-btn');
+let backspace = document.querySelector('.backspace');
+let number1 = '', number2 = '';
+let operator;
 
-// function isEmpty(obj){
-//     for(let key in obj){
-//         return false;
-//     }
-//     return true;
-// };
-
-// console.log(isEmpty(schedule));
-
-//calculator
-// function adding(num, num2){return parseInt(num) + parseInt(num2);}
-// function substraction(num, num2){return parseInt(num) - parseInt(num2);}
-// function multiply(num, num2){return parseInt(num) * parseInt(num2);}
-// function division(num, num2){return parseInt(num) / parseInt(num2);}
-
-// let number, operator, number2;
-
-// const numObject = {
-//     '+': adding(number, number2), 
-//     '-': substraction(number, number2), 
-//     '*': multiply(number, number2),
-//     '/': division(number, number2)
-// }
-
-// do{
-//     while(number != '=' || operator != '=' || number2 != '='){
-//     number = window.prompt("Enter a number: ");
-//     operator = window.prompt("Enter an operator: ");
-//     number2 = window.prompt("Enter a number: ");
-//     operator = window.prompt("Enter an operator: ");
-//     }
-//     console.log(numObject[operator]);
-// }while(number != 0);
-
-
-
-//first functions: add | substract | multiply | divide
-
+// adition functon expression
 let addition = function(num1, num2){
     return num1 + num2;
 };
 
+//subtraction function expression
+let subtraction = function(num1, num2){
+    return num1 - num2;
+};
+
+//multiplication function expression
 let multiply = function(num1, num2){
     return num1 * num2;
 };
 
-let substraction = function(num1, num2){
-    return num1 - num2;
+//division function expression
+let division = function(num1, num2){
+    if(num1 === 0 || num2 === 0){
+        alert('fuck u. No diving with zero.');
+    } else{
+        return num1 / num2;
+    }
 };
 
-let division = function(num1, num2){
-    return num1 / num2;
+//modulo function expression
+let modulo = function(num1, num2){
+    if(num1 === 0 || num2 === 0){
+        alert('fuck u. No diving with zero.');
+    } else{
+        return (num1 - (Math.floor(num1 / num2) * num2));
+    }
 };
+
+// function that decides which operation should take place depending on the entered operator
+let operate = function(num1, num2, operator){
+    if(operator === '+'){
+        return addition(num1, num2);
+    } else if(operator === '-'){
+        return subtraction(num1, num2);
+    } else if(operator === '*'){
+        return multiply(num1, num2);
+    } else if(operator === '%'){
+        return modulo(num1, num2);
+    } else return division(num1, num2);
+};
+
+// function that displays value of button
+let displayValue = function(value){
+    display.textContent += ` ${value} `;
+    if(value.charCodeAt(0) === 8){
+        display.textContent = 'potato';                                /////////////////////////////
+    } else if(value.charCodeAt(0) >= 48 && value.charCodeAt(0) <= 57){
+        if(number1 === ''){
+            number1 = value;
+            console.log('number1: ' + number1);
+        } else{
+            number2 += value;
+            console.log('number2: ' + number2);
+        }
+    } else if((value.charCodeAt(0) === 37) || (value.charCodeAt(0) >= 42 && value.charCodeAt(0) <= 47)){
+        operator = value;
+        console.log('operator: ' + operator);
+    } else if(value.charCodeAt(0) === 61){
+        display.textContent = operate(parseInt(number1), parseInt(number2), operator);
+        number1 = display.textContent;
+        console.log('num1: ' + number1);
+        if(number2){
+            number2 = '';
+        }
+        console.log('num2: ' + number2);
+    } else if(clear){
+        display.textContent = '';
+        number1 = '';
+        number2 = '';
+        operator = '';
+    } else {console.log('no condition match' + value);}
+};
+
+
+// function that attaches event listener to all buttons
+parent.addEventListener("click", (event) => {
+    if(event.target.tagName === 'BUTTON') {
+      displayValue(event.target.innerText);
+    };
+});
+
+clear.addEventListener('click', () => {                   /////////////////
+
+})
